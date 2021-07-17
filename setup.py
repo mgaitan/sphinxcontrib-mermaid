bbf66ac24adeb972bb9e3cc4d0b1f99a7a231ae7
Martín Gaitán <gaitan@gmail.com>
2021-07-17 20:27:34 -0300

    bump 0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 readme = io.open('README.rst', encoding="utf-8").read()
 changes = io.open('CHANGELOG.rst', encoding="utf-8").read()
-version = '0.7'
+version = '0.7.1'
 
 
 def long_description():
