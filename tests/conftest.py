f48589c231f9e0995bb6d6ff0e34021993cdf393
Nick Pilon <npilon@lexmachina.com>
2024-06-07 15:52:10 -0300

    Fix sphinx.testing deprecation warning

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,16 @@
+from pathlib import Path
+
 import pytest
-from sphinx.testing.path import path
+import sphinx
+from packaging.version import Version
+
 
 pytest_plugins = 'sphinx.testing.fixtures'
 
 
 @pytest.fixture(scope='session')
 def rootdir():
-    return path(__file__).parent.abspath() / 'roots'
+    if Version(sphinx.__version__) < Version('7.0.0'):
+        from sphinx.testing.path import path
+        return path(__file__).parent.abspath() / 'roots'
+    return Path(__file__).parent.absolute() / 'roots'
